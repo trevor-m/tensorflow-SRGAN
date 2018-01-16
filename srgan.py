Various bug fixes. Update ops needed to be added to train step for batch normalization layer to work

srgan.py
@@ -15,20 +15,13 @@ class SRGanGenerator:
       print('Invalid content loss function. Must be \'mse\', \'vgg22\', or \'vgg54\'.')
       exit()
     self.content_loss = content_loss
-    
-  def PReLU(self, x, scope=None):
-    with tf.variable_scope(name_or_scope=scope, default_name="prelu"):
-      alphas = tf.get_variable('alpha', x.get_shape()[-1], initializer=tf.constant_initializer(0.0), dtype=tf.float32)
-      pos = tf.nn.relu(x)
-      neg = alphas * (x - abs(x)) * 0.5
-      return pos + neg
 
   def ResidualBlock(self, x, kernel_size, filters, strides=1):
     """Residual block a la ResNet"""
     skip = x
     x = tf.layers.conv2d(x, kernel_size=kernel_size, filters=filters, strides=strides, padding='same')
     x = tf.layers.batch_normalization(x, training=self.training)
-    x = self.PReLU(x)
+    x = tf.contrib.keras.layers.PReLU(shared_axes=[1,2])(x)
     x = tf.layers.conv2d(x, kernel_size=kernel_size, filters=filters, strides=strides, padding='same')
     x = tf.layers.batch_normalization(x, training=self.training)
     x = x + skip
@@ -38,14 +31,14 @@ class SRGanGenerator:
     """Upsample 2x via SubpixelConv"""
     x = tf.layers.conv2d(x, kernel_size=kernel_size, filters=filters, strides=strides, padding='same')
     x = tf.depth_to_space(x, 2)
-    x = self.PReLU(x)
+    x = tf.contrib.keras.layers.PReLU(shared_axes=[1,2])(x)
     return x
 
   def forward(self, x):
     """Builds the forward pass network graph"""
     with tf.variable_scope('generator') as scope:
       x = tf.layers.conv2d(x, kernel_size=9, filters=64, strides=1, padding='same')
-      x = self.PReLU(x)
+      tf.contrib.keras.layers.PReLU(shared_axes=[1,2])(x)
       skip = x
 
       # B x ResidualBlocks
@@ -69,10 +62,10 @@ class SRGanGenerator:
       return tf.reduce_mean(tf.square(y - y_pred), name='content_loss')
     if self.content_loss == 'vgg22':
       # TODO
-      return 0
+      return 0 # * 1.0/12.75
     if self.content_loss == 'vgg54':
       # TODO
-      return 0
+      return 0 # * 1.0/12.75
 
   def _adversarial_loss(self, y_pred):
     """GAN"""
@@ -91,7 +84,10 @@ class SRGanGenerator:
   
   def optimize(self, loss):
     # TODO limit variables trained to only generator
-    return tf.train.AdamOptimizer(self.learning_rate).minimize(loss)
+    # update need to be added for batch normalization
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+      return tf.train.AdamOptimizer(self.learning_rate).minimize(loss)
 
 
 class SRGanDiscriminator:
@@ -143,4 +139,6 @@ class SRGanDiscriminator:
 
   def optimize(self, loss):
     # TODO limit variables trained
-    return tf.train.AdamOptimizer(self.learning_rate).minimize(loss)
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+      return tf.train.AdamOptimizer(self.learning_rate).minimize(loss)

train.py
@@ -23,7 +23,7 @@ def main():
   args = parser.parse_args()
   
   # Set up models
-  training = tf.placeholder(tf.bool)
+  training = tf.placeholder(tf.bool, name='training')
   discriminator = srgan.SRGanDiscriminator(training=training)
   generator = srgan.SRGanGenerator(discriminator=discriminator, training=training, learning_rate=args.learning_rate, content_loss=args.content_loss, use_gan=args.use_gan)
   # Generator
@@ -47,8 +47,8 @@ def main():
 
   with tf.Session() as sess:
     # test
-    op = sess.graph.get_operations()
-    [print(m.values()) for m in op if 'generator' in m.name]
+    #op = sess.graph.get_operations()
+    #[print(m.values()) for m in op if 'generator' in m.name]
     
     # Build input pipeline
     get_train_batch, get_val_batch, get_eval_batch, val_data, eval_data = build_inputs(args, sess)
@@ -71,8 +71,8 @@ def main():
       if iteration % args.log_freq == 0:
         val_error = evaluate_model(g_loss, get_val_batch, sess, args.num_test, args.batch_size)
         eval_error = evaluate_model(g_loss, get_eval_batch, sess, args.num_test, args.batch_size)
-        test_examples(val_data, g_y_pred, iteration, log_path, 'val')
-        test_examples(val_data, g_y_pred, iteration, log_path, 'eval')
+        test_examples(sess, val_data, g_y_pred, iteration, log_path, 'val')
+        test_examples(sess, eval_data, g_y_pred, iteration, log_path, 'eval')
         # Log error
         print('[%d] Test: %.7f, Train: %.7f' % (iteration, val_error, eval_error))
         with open(log_path + '/loss.csv', 'a') as f:

utilities.py
@@ -7,6 +7,8 @@ import datetime
 import os
 import shutil
 import math
+from scipy import misc
+import scipy.ndimage
 
 def process_individual_image(filename_queue, img_size, random_crop=False):
   """Individual loading & processing for each image"""
@@ -50,7 +52,7 @@ def build_inputs(args, sess):
     train_filenames = np.array(['overfit.png'])
     val_filenames = np.array(['overfit.png'])
     eval_filenames = np.array(['overfit.png'])
-    #args.batch_size = 1
+    args.batch_size = 1
     args.num_test = 1
   else:
     # Regular dataset
@@ -67,7 +69,7 @@ def build_inputs(args, sess):
   eval_data = create_tensor_from_files(eval_filenames[:5], sess, img_size=args.image_size_test)
 
   # Create input pipelines
-  get_train_batch = build_input_pipeline(train_filenames, batch_size=args.batch_size, img_size=args.image_size_train, random_crop=True)
+  get_train_batch = build_input_pipeline(train_filenames, batch_size=args.batch_size, img_size=args.image_size_train, random_crop=False)
   get_val_batch = build_input_pipeline(val_filenames, batch_size=args.batch_size, img_size=args.image_size_train)
   get_eval_batch = build_input_pipeline(eval_filenames, batch_size=args.batch_size, img_size=args.image_size_train)
   return get_train_batch, get_val_batch, get_eval_batch, val_data, eval_data
@@ -140,19 +142,19 @@ def evaluate_model(loss_function, get_batch, sess, num_images, batch_size):
     batch_hr = sess.run(get_batch)
     batch_lr = downsample_batch(batch_hr, factor=4)
     batch_lr, batch_hr = preprocess(batch_lr, batch_hr)
-    loss += sess.run(loss_function, feed_dict={training: False, g_x: batch_lr, g_y: batch_hr})
+    loss += sess.run(loss_function, feed_dict={'training:0': False, 'input_lowres:0': batch_lr, 'input_highres:0':batch_hr})
     total += 1
   loss = loss / total
   return loss
 
-def test_examples(batch_hr, g_y_pred, epoch, log_path, prefix, num=5):
+def test_examples(sess, batch_hr, g_y_pred, epoch, log_path, prefix, num=5):
   """Test the first num(5) images and save the outputs"""
   # make feed dict with downsampled images
   batch_lr = downsample_batch(batch_hr, factor=4)
   batch_lr, batch_hr = preprocess(batch_lr, batch_hr)
 
   # run everything through network
-  output = sess.run(g_y_pred, feed_dict={training: False, g_x: batch_lr, g_y: batch_hr})
+  output = sess.run(g_y_pred, feed_dict={'training:0': False, 'input_lowres:0': batch_lr})
   
   # iterate over all 5 examples
   for i in range(output.shape[0]):